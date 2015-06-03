035af7fb03613a9820210a46b8b712325325a302
MrWolfZ <ziller.jonathan@web.de>
2015-06-03 10:54:08 +0200

    cleanup

--- app/templates/src/app-ts/core/config/angular.config.ts
+++ app/templates/src/app-ts/core/config/angular.config.ts
@@ -9,33 +9,39 @@ module <%= prompts.prefix %>.core.config {
   /**
    * Enable debug level messages
    */
-  var LogConfig = ($logProvider: ng.ILogProvider) => {
+  var logConfig = ($logProvider: ng.ILogProvider) => {
     if ($logProvider.debugEnabled) {
       $logProvider.debugEnabled(true);
     }
   };
 
+  logConfig.$inject = ['$logProvider'];
+
   /**
    * Allows the framework to store the sails cookie from the backend and disable IE ajax request caching
    */
-  var HttpConfig = ($httpProvider: ng.IHttpProvider) => {
+  var httpConfig = ($httpProvider: ng.IHttpProvider) => {
     $httpProvider.defaults.withCredentials = true;
     if ($httpProvider.defaults.headers['get']) {
       $httpProvider.defaults.headers['get']['If-Modified-Since'] = '0';
     }
   };
 
+  httpConfig.$inject = ['$httpProvider'];
+
   /**
    * Tools like Protractor and Batarang need this information to run, but you can disable
    * this in production for a significant performance boost with
    */
-  var CompileConfig = ($compileProvider: ng.ICompileProvider) => {
+  var compileConfig = ($compileProvider: ng.ICompileProvider) => {
     $compileProvider.debugInfoEnabled(false);
   };
 
+  compileConfig.$inject = ['$compileProvider'];
+
   angular
     .module('<%= prompts.prefix %>.core.config.Angular', [])
-    .config(LogConfig)
-    .config(HttpConfig)
-    .config(CompileConfig);
+    .config(logConfig)
+    .config(httpConfig)
+    .config(compileConfig);
 }
