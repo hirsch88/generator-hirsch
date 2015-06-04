0c6dcb2285b857beaef08a53139c5928d0ff0987
MrWolfZ <ziller.jonathan@web.de>
2015-06-04 17:02:54 +0200

    fixed app miday spec to have proper test descriptions

--- app/templates/test-ts/midway/app.spec.ts
+++ app/templates/test-ts/midway/app.spec.ts
@@ -16,18 +16,10 @@ module <%= prompts.prefix %>.test {
         var hasModule = m => deps.indexOf(m) >= 0;
         before(() => deps = module.value('appName').requires);
 
-        //you can also test the module's dependencies
-        it('should have app.core as a dependency', () => {
-          expect(hasModule('<%= prompts.prefix %>.core')).to.equal(true);
-        });
-
-        it('should have app.config as a dependency', () => {
-          expect(hasModule('<%= prompts.prefix %>.home')).to.equal(true);
-        });
-
-        it('should have app.util as a dependency', () => {
-          expect(hasModule('<%= prompts.prefix %>.layout')).to.equal(true);
-        });
+        // you can also test the module's dependencies
+        it('should have <%= prompts.prefix %>.core as a dependency', () => expect(hasModule('<%= prompts.prefix %>.core')).to.equal(true));
+        it('should have <%= prompts.prefix %>.home as a dependency', () => expect(hasModule('<%= prompts.prefix %>.home')).to.equal(true));
+        it('should have <%= prompts.prefix %>.layout as a dependency', () => expect(hasModule('<%= prompts.prefix %>.layout')).to.equal(true));
       });
     });
   });
