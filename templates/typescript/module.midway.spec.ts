79ece3ade43958cceae7aa5dcf89a82feea3fc3d
MrWolfZ <ziller.jonathan@web.de>
2015-06-04 17:03:11 +0200

    added module dependency checks to module miday spec template

--- templates/typescript/module.midway.spec.ts
+++ templates/typescript/module.midway.spec.ts
@@ -4,9 +4,29 @@ module <%= prefix %>.<%= cameledName %>.test {
   'use strict';
 
   describe('Midway: <%= prefix %>.<%= cameledName %>', () => {
+    var app: ng.IModule;
+    var appDeps: string[];
     var module: ng.IModule;
-    before(() => module = angular.module('<%= prefix %>.<%= cameledName %>'));
+    var appHasModule = m => appDeps.indexOf(m) >= 0;
+
+    before(() => {
+      app = angular.module('<%= prefix %>');
+      appDeps = app.value('<%= prefix %>').requires;
+      module = angular.module('<%= prefix %>.<%= cameledName %>');
+    });
 
     it('should be registered', () => should.exist(module));
+
+    it('should be registered in the app module', function () {
+      expect(appHasModule('<%= prefix %>.<%= cameledName %>')).to.equal(true);
+    });
+
+    describe('Dependencies: ', () => {
+      var deps: string[];
+      var hasModule = m => deps.indexOf(m) >= 0;
+      before(() => deps = module.value('<%= prefix %>.<%= cameledName %>').requires);
+
+      // it('should have <%= prefix %>.<%= cameledName %>.Nobody as a dependency', () => expect(hasModule('<%= prefix %>.<%= cameledName %>.Nobody')).to.equal(true));
+    });
   });
 }
