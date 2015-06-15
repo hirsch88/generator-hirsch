50ed32d89056a0316c8739dd101a05e41b3d9eca
MrWolfZ <ziller.jonathan@web.de>
2015-06-15 18:12:09 +0200

    changed service component generator to use ID constants as module names for services

--- templates/typescript/services.module.ts
+++ templates/typescript/services.module.ts
@@ -5,14 +5,14 @@ module <%= prefix %>.<%= module %>.services {
 
   export var Namespace = '<%= prefix %>.<%= module %>.services';
 
-  angular
-    .module(Namespace, [<% for (var i = 0, l = components.length; i < l; i++) { %>
-      `${Namespace}.<%= components[i] %>`, <% } %>
-      `${Namespace}.<%= classedName %>Service`
-    ]);
-
   export var ID = {<% for (var i = 0, l = components.length; i < l; i++) { %>
     <%= components[i] %>: `${Namespace}.<%= components[i] %>`, <% } %>
     <%= classedName %>Service: `${Namespace}.<%= classedName %>Service`
   };
+
+  angular
+    .module(Namespace, [<% for (var i = 0, l = components.length; i < l; i++) { %>
+      ID.<%= components[i] %>, <% } %>
+      ID.<%= classedName %>Service
+    ]);
 }
