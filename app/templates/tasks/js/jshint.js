3117f57cf8fadf87511298596f51f74de771c7f3
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2015-07-24 16:19:54 +0200

    fix(jshint)

--- app/templates/tasks/js/jshint.js
+++ app/templates/tasks/js/jshint.js
@@ -2,17 +2,17 @@
 
 var gulp = require('gulp');
 var projectConfig = require(process.cwd() + '/project.config.js')();
-var $ = require('gulp-load-plugins')({lazy: true});
+var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
 /**
  * JSHINT
  * Checks the source code with some defined guidelines from the .jshintrc
  */
-gulp.task('jshint', function () {
+gulp.task('jshint', function () {	
   return gulp
     .src(projectConfig.path.srcDir + '/' + projectConfig.path.app.scripts)
-    .pipe($.jshint())
-    .pipe($.jshint.reporter(stylish))
-    .pipe($.jshint.reporter('fail'));
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail'));
 });
