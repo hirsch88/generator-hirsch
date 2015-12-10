1642ac01c97b9296271d4517ce0438854c18fcbb
Jonathan Ziller <jonathan.ziller@gmail.com>
2015-12-10 17:09:13 +0100

    refactor(): added tslint exception to unit tests for directives

--- templates/typescript/directive.unit.spec.ts
+++ templates/typescript/directive.unit.spec.ts
@@ -8,8 +8,10 @@ namespace <%= prefix %>.<%= module %>.<%= $namespace %>.test {
 
     beforeEach(angular.mock.module(`${Namespace}.<%= classedName %>`));
 
+    /* tslint:disable:variable-name */
     beforeEach(angular.mock.inject(_$compile_ => $compile = _$compile_));
-    beforeEach(angular.mock.inject(_$rootScope_ => $rootScope = _$rootScope_));<% if (hasController) { %>
+    beforeEach(angular.mock.inject(_$rootScope_ => $rootScope = _$rootScope_));
+    /* tslint:enable:variable-name */<% if (hasController) { %>
 
     let controller: <%= classedName %>Controller;
     beforeEach(inject(($controller: ng.IControllerService) => controller = $controller(ID.<%= classedName %>Controller)));
